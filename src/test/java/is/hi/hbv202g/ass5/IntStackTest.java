d3efa66b8f3fb071098607a6807a71e62d48c021
dannith <dannitorr@gmail.com>
2023-02-21 10:43:13 +0000

    Questions 4-7

--- src/test/java/is/hi/hbv202g/ass5/IntStackTest.java
+++ src/test/java/is/hi/hbv202g/ass5/IntStackTest.java
@@ -7,11 +7,42 @@ import org.junit.Test;
 
 public class IntStackTest {
 
+    private IntStack stack;
+
+    @Before
+    public void createTestStack(){
+        stack = new IntStack();
+    }
     @Test
     public void testNewStackIsNotFull() {
-        IntStack stack= new IntStack();
-
         assertFalse(stack.isFull());
+    }
+
+    @Test
+    public void testFullStackIsFull(){
+        for(int i = 0; i < stack.getCapacity(); i++)
+            stack.push(i);
+        assertTrue(stack.isFull());
+    }
+
+    @Test
+    public void testAlmostFullStackIsNotFull(){
+        for(int i = 0; i < stack.getCapacity() - 1; i++)
+            stack.push(i);
+        assertFalse(stack.isFull());
+    }
+
+    @Test
+    public void testAlmostEmptyStackIsNotFull(){
+        stack.push(1);
+        assertFalse(stack.isFull());
+    }
+
+    @Test
+    public void testPopReturnsPushedValue(int tala){
+        stack.push(tala);
+        int poppedTala = stack.pop();
+        assertEquals(tala, poppedTala);
     }
 
 }
